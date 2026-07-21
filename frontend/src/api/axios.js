6bd5791a4b5921d6642c3ce0a1085cbcdaccc060
Riya Gaud <rayagaud7@gmail.com>
2026-07-22 01:57:01 +0530

    Configure frontend for Railway

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: `${import.meta.env.VITE_API_URL}`,
 })
 
 // attach access token to every request
